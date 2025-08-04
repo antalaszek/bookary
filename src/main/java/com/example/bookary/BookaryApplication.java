166a79a3cad5e922b39dcab0e3f5a6c6662be4f0
Antek Moszynski <antalaszek@gmail.com>
2025-08-04 13:48:35 +0200

    initial commit

---
+++ src/main/java/com/example/bookary/BookaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookary/BookaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
